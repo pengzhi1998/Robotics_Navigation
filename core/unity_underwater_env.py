2e6ae4cc34bccafeac27868c38fdee0414f1b0e0
pengzhi1998 <tyypz2590477658@gmail.com>
2022-01-06 02:12:13 +0800

    adjust water a little bit

--- core/unity_underwater_env.py
+++ core/unity_underwater_env.py
@@ -220,10 +220,11 @@ class Underwater_navigation():
         obs_ray = np.array([np.min([obs_img_ray[1][1], obs_img_ray[1][3], obs_img_ray[1][5]]) * 10 * 0.5])
         obs_goal_depthfromwater = np.array(self.pos_info.goal_depthfromwater_info())
         if self.training == False:
-            visibility = random.uniform(5, 20)
-            self.pos_info.assign_testpos_visibility(self.start_goal_pos + [20])
+            visibility = random.uniform(5, 25)
+            self.pos_info.assign_testpos_visibility(self.start_goal_pos + [visibility])
         else:
-            self.pos_info.assign_testpos_visibility([0] * 8 + [20])
+            visibility = random.uniform(5, 25)
+            self.pos_info.assign_testpos_visibility([0] * 9 + [visibility])
 
 
         # construct the observations of depth images, goal infos, and rays for consecutive 4 frames
