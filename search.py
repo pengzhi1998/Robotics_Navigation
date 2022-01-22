55fa7f93f71e1d3201196dc0a3ce13bc6f4bcd95
pengzhi1998 <tyypz2590477658@gmail.com>
2022-01-22 11:03:59 +0800

    search

--- search.py
+++ search.py
@@ -1,25 +1,39 @@
 import torch
-import torch.distributions.normal as normal
+import torch.distributions.normal as norm
 import torch.nn as nn
 import numpy as np
 
-data = np.array([[0, 31], [-0.1, 32], [0.05, 33], [-0.5, -5], [0.4, 2],
+data = torch.tensor([[0, 31], [-0.1, 32], [0.05, 33], [-0.5, -5], [0.4, 2],
               [-1, -19], [-0.8, -20], [0.7, -3], [0.8, -22], [1, -22]])
-data_visibility = data[:, 0]
+data_visibility = data[:, 0] - 1
 data_reward = data[:, 1]
-
-N = normal.Normal(0, 1)
+tem = 1
+learning_rate = 5e-2 # Adam
 
 class Gaussian(nn.Module):
     def __init__(self):
-        self.dis = normal.Normal()
-    def forward(self):
-        return
+        super().__init__()
+        self.mean = nn.Parameter(torch.Tensor([0]), requires_grad=True)
+        self.std = nn.Parameter(torch.Tensor([1]), requires_grad=True)
 
+    def forward(self, x):
+        # normal = torch.distributions.Normal(self.mean, self.std)
+        normal = norm.Normal(self.mean, self.std)
+        return normal.log_prob(x)
 
-for i in range(10):
-    loss = -N.log_prob(torch.tensor(data_visibility)) * torch.tensor(data_reward)
+    def get_meanstd(self):
+        return self.mean, self.std
+
+Gaussian = Gaussian()
+optimizer = torch.optim.Adam(Gaussian.parameters(), lr=learning_rate)
+value = torch.mean(data_reward)
+
+for i in range(50):
+    loss = 0
+    for i in range(len(data_visibility)):
+        loss -= Gaussian(data_visibility[i]) * torch.exp((data_reward[i] - value)/tem)
+    optimizer.zero_grad()
     loss.backward()
+    optimizer.step()
 
-
-
+print(Gaussian.get_meanstd())
