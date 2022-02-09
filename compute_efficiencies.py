221ac031f6604244323dc44adee8c915d775f449
pengzhi1998 <tyypz2590477658@gmail.com>
2022-02-09 11:35:20 +0800

    write a script for data processing

---
+++ compute_efficiencies.py
@@ -0,0 +1,94 @@
+import os
+from utils import *
+import numpy as np
+
+my_open = open(os.path.join(assets_dir(), 'learned_models/test_efficiency.txt'), "r")
+lines = my_open.readlines()
+whole_data = []
+data_onemodel = []
+data_onetrajectory = []
+t = 0
+
+for line in lines:
+    if line.split():
+        if 'with' in line and t!=0:
+            whole_data.append(data_onemodel)
+            data_onemodel = []
+        elif t!=0:
+            line = line.strip("\n").split()
+            data_onetrajectory.append(line)
+    else:
+        if data_onetrajectory[0][1] == 'failed':
+            pass
+        else:
+            data_onemodel.append(data_onetrajectory)
+        data_onetrajectory = []
+    t += 1
+data_onemodel.append(data_onetrajectory)
+whole_data.append(data_onemodel)
+print(np.shape(whole_data), whole_data)
+
+data = []
+for data_onemodel in whole_data:
+    for wp in range(5):
+        data_model_wp = []
+        for data_onetrajectory in data_onemodel:
+            if wp == 0:
+                data_piece = float(data_onetrajectory[wp][0])
+            else:
+                data_piece = float(data_onetrajectory[wp][0]) - float(data_onetrajectory[wp-1][0])
+            data_model_wp.append(data_piece)
+        print(np.round(np.mean(data_model_wp), 2), np.round(np.std(data_model_wp), 2))
+        data.append(data_model_wp)
+
+print(data)
+
+
+
+
+# _plotdata = []
+#
+# t = 1
+# for data in whole_data:
+#     succ_num = 0.
+#     steps = []
+#     for i in range(len(data)):
+#         if data[i][0] == 'success':
+#             succ_num += 1.
+#             steps.append(float(data[i][1]))
+#     if len(steps) == 0:
+#         steps.append(0)
+#     successful_ratio = succ_num/10.0
+#     rewards = [float(i[2]) for i in data]
+#     print(t, np.round(np.mean(rewards), 2), np.round(np.std(rewards), 2), np.round(np.mean(steps), 2),
+#         np.round(np.std(steps), 2), successful_ratio)
+#     _plotdata.append([
+#         t, np.round(np.mean(rewards), 2), np.round(np.std(rewards), 2), np.round(np.mean(steps), 2),
+#         np.round(np.std(steps), 2), successful_ratio * 100
+#     ])
+#     t += 1
+#
+# plot_data = []
+# for i in range(len(_plotdata)):
+#     if i%3 == 0:
+#         plot_data.append(_plotdata[i])
+#
+# for i in range(len(_plotdata)):
+#     if i%3 == 1:
+#         plot_data.append(_plotdata[i])
+#
+# for i in range(len(_plotdata)):
+#     if i%3 == 2:
+#         plot_data.append(_plotdata[i])
+#
+# for i in range(len(plot_data)):
+#     print(i+1, ":", plot_data[i])
+
+# withoutechosounder = [540, 548, 543, 585, 501, 594] # 60%
+# withechosounder = [463, 465, 466, 453, 460, 457, 467, 466, 467, 464]
+# bug2 = [734, 729, 729, 729, 729, 729, 729, 729, 729, 729]
+#
+# mean_withoutechosounder, std_withoutechosounder = np.mean(withoutechosounder), np.std(withoutechosounder)
+# mean_withechosounder, std_withechosounder = np.mean(withechosounder), np.std(withechosounder)
+# mean_bug2, std_bug2 = np.mean(bug2), np.std(bug2)
+# print(mean_withoutechosounder, std_withoutechosounder, mean_withechosounder, std_withechosounder, mean_bug2, std_bug2)
---
+++ plot_efficiency.py
@@ -0,0 +1,175 @@
+# refer from: https://blog.csdn.net/weixin_43790779/article/details/110143115
+import os
+from utils import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
+my_open = open(os.path.join(assets_dir(), 'learned_models/test_pos_plot_efficiency.txt'), "r")
+lines = my_open.readlines()
+whole_data = []
+data_onemodel = []
+data_onetrajectory = []
+t = 0
+
+for line in lines:
+    if line.split():
+        if 'with' in line and t!=0:
+            whole_data.append(data_onemodel)
+            data_onemodel = []
+        elif t!=0:
+            line = line.strip("\n").split()
+            data_onetrajectory.append([float(line[0]), float(line[1])])
+    else:
+        data_onemodel.append(data_onetrajectory)
+        data_onetrajectory = []
+    t += 1
+data_onemodel.append(data_onetrajectory)
+whole_data.append(data_onemodel)
+
+path_withechosounder = whole_data[0]
+path_bug2 = whole_data[1]
+path_withoutechosounder = whole_data[2]
+colors = ['dodgerblue', 'g', 'm', 'r', 'c']
+
+t = 0
+for wp in path_withechosounder:
+    for i in range(len(wp) - 1):
+        start = (wp[i][0], wp[i + 1][0])
+        end = (wp[i][1], wp[i + 1][1])
+        plt.plot(start, end, linewidth=2., color=colors[t])
+    t += 1
+
+''' goals '''
+plt.plot(5, -5, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(4, -6, 1, color='k', fontsize=12)
+plt.plot(-10, 0, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, -1, 2, color='k', fontsize=12)
+plt.plot(-10, 10, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, 9, 3, color='k', fontsize=12)
+plt.plot(-20, 20, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-21, 19, 4, color='k', fontsize=12)
+plt.plot(-10, 50, color='k', marker='o', markerfacecolor='yellow', markersize=80)
+plt.text(-11, 49, 5, color='k', fontsize=20)
+
+''' initial position '''
+plt.plot(14, -15, color='red', marker='<', markersize=8)
+
+''' obstacles '''
+plt.plot(11.2, -10.3, color='k', marker='o', markersize=8)
+plt.plot(-3.01, -12.88, color='k', marker='o', markersize=8)
+plt.plot(-12.96, -9.97, color='k', marker='o', markersize=8)
+plt.plot(1.65, -4.75, color='k', marker='o', markersize=8)
+plt.plot(-9.58, -2.21, color='k', marker='o', markersize=8)
+plt.plot(-19.93, -3.11, color='k', marker='o', markersize=8)
+plt.plot(8.65, 3.82, color='k', marker='o', markersize=8)
+plt.plot(-2.53, 4.18, color='k', marker='o', markersize=8)
+plt.plot(-20.13, 12.2, color='k', marker='o', markersize=8)
+plt.plot(-14.07, 13.6, color='k', marker='o', markersize=8)
+plt.plot(0.43, 14.04, color='k', marker='o', markersize=8)
+plt.plot(7.73, 10.07, color='k', marker='o', markersize=8)
+plt.plot(14.19, 13.17, color='k', marker='o', markersize=8)
+
+d_line = mlines.Line2D([], [], color='dodgerblue',
+                          markersize=15, label='Path 1')
+g_line = mlines.Line2D([], [], color='g',
+                          markersize=15, label='Path 2')
+m_line = mlines.Line2D([], [], color='m',
+                          markersize=15, label='Path 3')
+r_line = mlines.Line2D([], [], color='r',
+                          markersize=15, label='Path 4')
+c_line = mlines.Line2D([], [], color='c',
+                          markersize=15, label='Path 5')
+plt.legend(handles=[d_line, g_line, m_line, r_line, c_line])
+
+plt.axis('scaled')
+plt.xticks(size=12)
+plt.yticks(size=12)
+plt.grid(axis='x')
+plt.grid(axis='y')
+plt.savefig("./assets/learned_models/withechosounder.pdf")
+plt.show()
+
+t = 0
+for wp in path_withoutechosounder:
+    for i in range(len(wp) - 1):
+        start = (wp[i][0], wp[i + 1][0])
+        end = (wp[i][1], wp[i + 1][1])
+        plt.plot(start, end, linewidth=2., color=colors[t])
+    t += 1
+plt.plot(5, -5, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(4, -6, 1, color='k', fontsize=12)
+plt.plot(-10, 0, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, -1, 2, color='k', fontsize=12)
+plt.plot(-10, 10, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, 9, 3, color='k', fontsize=12)
+plt.plot(-20, 20, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-21, 19, 4, color='k', fontsize=12)
+plt.plot(-10, 50, color='k', marker='o', markerfacecolor='yellow', markersize=80)
+plt.text(-11, 49, 5, color='k', fontsize=20)
+plt.plot(14, -15, color='red', marker='<', markersize=8)
+plt.plot(11.2, -10.3, color='k', marker='o', markersize=8)
+plt.plot(-3.01, -12.88, color='k', marker='o', markersize=8)
+plt.plot(-12.96, -9.97, color='k', marker='o', markersize=8)
+plt.plot(1.65, -4.75, color='k', marker='o', markersize=8)
+plt.plot(-9.58, -2.21, color='k', marker='o', markersize=8)
+plt.plot(-19.93, -3.11, color='k', marker='o', markersize=8)
+plt.plot(8.65, 3.82, color='k', marker='o', markersize=8)
+plt.plot(-2.53, 4.18, color='k', marker='o', markersize=8)
+plt.plot(-20.13, 12.2, color='k', marker='o', markersize=8)
+plt.plot(-14.07, 13.6, color='k', marker='o', markersize=8)
+plt.plot(0.43, 14.04, color='k', marker='o', markersize=8)
+plt.plot(7.73, 10.07, color='k', marker='o', markersize=8)
+plt.plot(14.19, 13.17, color='k', marker='o', markersize=8)
+
+plt.axis('scaled')
+plt.xticks(size=12)
+plt.yticks(size=12)
+plt.grid(axis='x')
+plt.grid(axis='y')
+plt.savefig("./assets/learned_models/withoutechosounder.pdf")
+plt.show()
+
+t = 0
+for wp in path_bug2:
+    for i in range(len(wp) - 1):
+        start = (wp[i][0], wp[i + 1][0])
+        end = (wp[i][1], wp[i + 1][1])
+        plt.plot(start, end, linewidth=2., color=colors[t])
+    t += 1
+plt.plot(5, -5, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(4, -6, 1, color='k', fontsize=12)
+plt.plot(-10, 0, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, -1, 2, color='k', fontsize=12)
+plt.plot(-10, 10, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-11, 9, 3, color='k', fontsize=12)
+plt.plot(-20, 20, color='k', marker='o', markerfacecolor='yellow', markersize=15)
+plt.text(-21, 19, 4, color='k', fontsize=12)
+plt.plot(-10, 50, color='k', marker='o', markerfacecolor='yellow', markersize=80)
+plt.text(-11, 49, 5, color='k', fontsize=20)
+plt.plot(14, -15, color='red', marker='<', markersize=8)
+plt.plot(11.2, -10.3, color='k', marker='o', markersize=8)
+plt.plot(-3.01, -12.88, color='k', marker='o', markersize=8)
+plt.plot(-12.96, -9.97, color='k', marker='o', markersize=8)
+plt.plot(1.65, -4.75, color='k', marker='o', markersize=8)
+plt.plot(-9.58, -2.21, color='k', marker='o', markersize=8)
+plt.plot(-19.93, -3.11, color='k', marker='o', markersize=8)
+plt.plot(8.65, 3.82, color='k', marker='o', markersize=8)
+plt.plot(-2.53, 4.18, color='k', marker='o', markersize=8)
+plt.plot(-20.13, 12.2, color='k', marker='o', markersize=8)
+plt.plot(-14.07, 13.6, color='k', marker='o', markersize=8)
+plt.plot(0.43, 14.04, color='k', marker='o', markersize=8)
+plt.plot(7.73, 10.07, color='k', marker='o', markersize=8)
+plt.plot(14.19, 13.17, color='k', marker='o', markersize=8)
+
+plt.axis('scaled')
+plt.xticks(size=12)
+plt.yticks(size=12)
+plt.grid(axis='x')
+plt.grid(axis='y')
+plt.savefig("./assets/learned_models/withbug2.pdf")
+plt.show()
+
+
+
+
