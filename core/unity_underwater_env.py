2bd94a09bf5bd274d64004f03cf79bca05c0c133
pengzhi1998 <tyypz2590477658@gmail.com>
2022-02-10 21:14:56 +0800

    modify a little bit for video

--- core/unity_underwater_env.py
+++ core/unity_underwater_env.py
@@ -440,8 +440,8 @@ class Underwater_navigation():
         # print("goals:", self.obs_goals, "\nrays:", self.obs_rays, "\nactions:",
         #       self.obs_actions, "\nvisibility_Gaussian:", self.visibility_para_Gaussian, "\nreward:", reward)
 
-        # cv2.imwrite("img_rgb_step.png", 256 * cv2.cvtColor(obs_img_ray[0] ** 0.45, cv2.COLOR_RGB2BGR))
-        # cv2.imwrite("img_depth_pred_step.png", 256 * self.obs_preddepths[0])
+        # cv2.imwrite("img_rgb_step"+str(self.step_count)+".png", 256 * cv2.cvtColor(obs_img_ray[0] ** 0.45, cv2.COLOR_RGB2BGR))
+        # cv2.imwrite("img_depth_pred_step"+str(self.step_count)+".png", 256 * self.obs_preddepths[0])
 
         # if self.training == False:
         #     my_open = open(os.path.join(assets_dir(), 'learned_models/test_pos.txt'), "a")
