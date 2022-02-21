2c079d1b85e71b9fa31d715668cd4d5ccce60822
pengzhi1998 <tyypz2590477658@gmail.com>
2022-02-21 16:26:37 +0800

    data process

--- core/agent.py
+++ core/agent.py
@@ -122,7 +122,7 @@ def collect_samples(pid, queue, env, policy, custom_reward,
             for element in data:
                 my_open.write(element)
             my_open.close()
-            if num_episodes >= 100:
+            if num_episodes >= 101:
                 exit()
 
     print(time.time())
